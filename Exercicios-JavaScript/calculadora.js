e2a1f4fba3f0be9795012f384b35b7f41e3d3496
Igor Oliveira <igore.m.oliveira@gmail.com>
2022-02-01 20:34:49 -0300

    Update calculadora.js

--- Exercicios-JavaScript/calculadora.js
+++ Exercicios-JavaScript/calculadora.js
@@ -6,3 +6,12 @@ function somar(valor1=0, valor2=0) {
 }
 
 console.log('O resultado da função somar(5, 3) é = ', somar(5, 3))
+
+
+// Subtrair
+function menos(valor1=0, valor2=0) {
+    return valor1 - valor2
+}
+
+console.log('O resultado da função meno(5, 3) é = ', menos(5, 3))
+
