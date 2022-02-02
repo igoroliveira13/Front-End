d68a78768d1079c507233c6825af76bedb75e2f6
Igor Oliveira <igore.m.oliveira@gmail.com>
2022-02-02 20:33:52 -0300

    Update calculadora.js

--- Exercicios-JavaScript/calculadora.js
+++ Exercicios-JavaScript/calculadora.js
@@ -29,3 +29,18 @@ o usuario esqueça de digitar um numero ou não o digite intencionalmente, resul
 erro pois a função espera receber dois parametros, colocando =0 o código irá inserir 
 o zero no lugar de um parametro não inserido. 
 */
+
+
+// Dividir
+function dividi(dividendo, divisor) {
+    console.log('O resultado da função dividi(15, 3) é = ', dividi(15, 3)) 
+}
+
+// para a função dividir não é possivel utilizar o =0 para o divisor pois não é possivel dividir por zero. 
+
+
+// Exponenciação
+function exp(valor1, valor2) {
+    console.log('O resultado da função exp(5, 3) é = ', exp(5, 3))
+}
+
