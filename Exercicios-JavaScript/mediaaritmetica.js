ffdd513e57ca5d0168d4cd9a7e3b85c1808204f7
Igor Oliveira <igore.m.oliveira@gmail.com>
2022-02-04 18:24:07 -0300

    Create mediaaritmetica.js

---
+++ Exercicios-JavaScript/mediaaritmetica.js
@@ -0,0 +1,17 @@
+// Média aritmética de notas escolares 
+
+let nota1 = 9
+let nota2 = 7
+let media;
+
+if ((nota1 + nota2) / 2 > 7) {
+    console.log('Aprovado')
+} else {
+    console.log('Reprovado')
+}
+
+/* 
+Igual na matematica, é necesario colocar nota1 + nota2 entre 
+parenteses é essencial para que o código funcione corretamente.
+*/
+
